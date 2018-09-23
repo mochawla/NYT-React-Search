cc9a4967748287621fd84fa3730458eb988dcc6d
Mohammed Chawla <chawla.mohammed@gmail.com>
2018-09-23 17:53:47 -0500

    added node and mongoose connect code

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const path = require("path");
+const bodyParser = require("body-parser");
+const PORT = process.env.PORT || 3001;
+const app = express();
+const mongoose = require("mongoose");
+
+// Define middleware here
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
+// Connect to the Mongo DB
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/nytreact");
+
+// Define API routes here
+
+// Send every other request to the React app
+// Define any API routes before this runs
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
+
+app.listen(PORT, () => {
+  console.log(`🌎 ==> Server now on port ${PORT}!`);
+});
